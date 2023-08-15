c6d09fd279d2873113788b2697cffdce57fb12fd
Dirk W <dirk.woellhaf@gmail.com>
2023-08-15 14:43:19 +0200

    Get it running

--- src/main.go
+++ src/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
-	"io/ioutil"
-	"encoding/json"
+//	"net/http"
+//	"io/ioutil"
+//	"encoding/json"
 //	"os"
 //	"strings"
 	//"flag"
 //	"time" 
-	"strconv"
+//	"strconv"
 	"github.com/spf13/viper"
 )
 
@@ -22,9 +22,10 @@ func Logger(msg string){
 
 
 func main()  {
-	viper.SetConfigName("tedot") // name of config file (without extension)
+	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("toml") // REQUIRED if the config file does not have the extension in the name
 	viper.AddConfigPath(".")               // optionally look for config in the working directory
+	viper.AddConfigPath("../")               // optionally look for config in the working directory
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil { // Handle errors reading the config file
 		panic(fmt.Errorf("Fatal error config file: %w \n", err))
